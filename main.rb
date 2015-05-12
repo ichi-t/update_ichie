975774a27d3f70e7b484a7f5b358558519119787
市村泰佑 <kurosanex@dot1x4078.14-224-20.wlan.ms.dendai.ac.jp>
2015-05-12 15:38:46 +0900

    first commit

---
+++ main.rb
@@ -0,0 +1,60 @@
+# coding: utf-8
+require 'rubygems'
+require 'twitter'
+require 'tweetstream'
+require './keys.rb'
+
+
+# -- Twitter ---
+
+TweetStream.configure do |config|
+    config.consumer_key       = CONSUMER_KEY
+    config.consumer_secret    = CONSUMER_SECRET
+    config.oauth_token        = ACCESS_TOKEN
+    config.oauth_token_secret = ACCESS_SECRET
+end
+
+restclient = Twitter::REST::Client.new do |config|
+    config.consumer_key        = CONSUMER_KEY 
+    config.consumer_secret     = CONSUMER_SECRET
+    config.access_token        = ACCESS_TOKEN
+    config.access_token_secret = ACCESS_SECRET
+end
+
+# client = TweetStream::Client.new.follow('185792744') do |status|
+client = TweetStream::Client.new.userstream do |status|
+  p status.text
+  text = status.text
+
+  if text.start_with? "RT","@","＠"
+    p "avoid"
+    next
+  elsif text =~ /[@＠]/
+    p "avoid"
+    next
+
+  elsif text.end_with? "!"
+    p "next"
+    next
+  elsif text.end_with? "っちー"
+    p "mutch"
+    # newname = text.gsub(/っちー/)
+
+    if text.length > 12 || status.user.screen_name == "Thunder_Pudding"
+      p "!nagai"
+      tweet = "@#{status.user.screen_name} だまれ"
+      restclient.update(tweet, :in_reply_to_status_id => status.id)
+      next
+    else
+      tweet = "@#{status.user.screen_name} #{text}ダヨ!"
+      restclient.update_profile({:name => text})
+      restclient.update(tweet, :in_reply_to_status_id => status.id)
+      next
+      # restclient.update(text + "ダヨ!")
+    end
+      
+  end
+end
+
+
+
